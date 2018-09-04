ea611dcde25a80074f0c343cb66449e1cb611cd4
razorheadfx <razorhead.effect@gmail.com>
2018-09-04 20:35:36 +0200

    WIP: start building out mdcr; switch to stable

--- src/bin/mdcr.rs
+++ src/bin/mdcr.rs
@@ -1,11 +1,14 @@
-#![feature(drain_filter)]
-use std::io;
-
 #[macro_use]
 extern crate log;
-extern crate mediocore;
 extern crate env_logger;
 
+#[macro_use]
+extern crate structopt;
+use structopt::StructOpt;
+
+extern crate mediocore;
+
+use std::io;
 
 macro_rules! ghz{
     ($x:expr) =>(   
@@ -14,17 +17,50 @@ macro_rules! ghz{
 )
 }
 
-	
-fn main() -> io::Result<()>{
-    env_logger::init();
+#[derive(Debug, StructOpt)]
+#[structopt(name = "mediocore")]
+enum Mdcr{
+	#[structopt(name = "help")]
+	Help,
+	#[structopt(name = "powersave")]
+	Powersave,
+	#[structopt(name = "performance")]
+	Performance,
+	#[structopt(name = "show"]
+	Show
+}
 
-    let mut cores = mediocore::discover_core_settings()?;
+fn discover() -> io::Result<Vec<CoreSetting>>{
+	let mut cores = mediocore::discover_core_settings()?;
     cores.sort_by_key(|c| c.num());
 	info!("Discovered Configuration {:#?}", cores);
+	Ok(cores)
+}
 
+fn powersave(){
+	unimplemented!()
+}
+
+fn performance(){
+	unimplemented!()
+}
+
+fn show(){
+	unimplemented!()
+}
 	
-
-
+fn main(){
+    env_logger::init();
+    let settings = Mdcr::from_args();
+    debug!("Args provided: {:#?}", settings)
+    
+    
+	match settings{
+			Help => unimplemented!(),
+			Powersave => powersave(),
+			Performance => performance(),
+			_ => unimplemented!()
+	}
 	 
     Ok(())
 }
